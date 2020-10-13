6c37c8991005b8ffe716944fefa517b9a3b3c383
Alex <alexsaha7@gmail.com>
2020-10-13 03:39:38 -0400

    data returned from api displays on webpage

--- src/app/weather-component/weather-component.component.ts
+++ src/app/weather-component/weather-component.component.ts
@@ -28,14 +28,10 @@ export class WeatherComponentComponent implements OnInit {
   getPost()  {
     this.http.get<any>("http://api.openweathermap.org/data/2.5/weather?q=" + this.city + "&appid=be6801d0b56818b9c53b46afef41f8da").subscribe(data => {
       
-      this.temp = data.main.temp
-      this.temp_max = data.main.temp_max
-      this.temp_min = data.main.temp_min
+      this.temp = ((data.main.temp) * 9/5) - 459.67
+      this.temp_max = ((data.main.temp_max) * 9/5) - 459.67
+      this.temp_min = ((data.main.temp_min) * 9/5) - 459.67
       this.condition = data.weather[0].main
-
-      alert(this.city + " Weather: Conditions: " + this.condition + " Temp: " + this.temp + " Max: " + this.temp_max + " Min: " + this.temp_min)
-      console.log(this.condition);
-
     })
   }
 
@@ -43,7 +39,6 @@ export class WeatherComponentComponent implements OnInit {
   }
 
   activateSearch() {
-    alert(this.city);
     this.getPost();
   }
 }
